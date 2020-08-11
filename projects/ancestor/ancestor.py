c209cc0dc7b9612ce5dc0925a5aed085554d99ef
aposipa <andrew.posipanka@gmail.com>
2020-08-11 17:09:43 -0400

    day2 mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,25 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    tree = Graph()
+    for (vert_1, vert_2) in ancestors:
+        tree.add_vertex(vert_1)
+        tree.add_vertex(vert_2)
+
+    for (vert_1, vert_2) in ancestors:
+        tree.add_edge(vert_1, vert_2)
+
+    target_vert = None
+    longest_path = 1
+
+    for vert in tree.vertices:
+        path = tree.dfs(vert, starting_node)
+
+        if path:
+            if len(path) > longest_path:
+                longest_path = len(path)
+                target_vert = vert
+        elif not path and longest_path == 1:
+            target_vert = -1
+
+    return target_vert
